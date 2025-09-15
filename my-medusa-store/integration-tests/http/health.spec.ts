21989bdd0f573a1c8a4b5ac9fafc0addbbec86a9
Aleksandergreg <alek3691@stud.kea.dk>
2025-09-15 08:35:35 +0200

    CI: Setup postgres DB on the github runner for making actual integrations tests aagainst our MCP server

--- my-medusa-store/integration-tests/http/health.spec.ts
+++ my-medusa-store/integration-tests/http/health.spec.ts
@@ -1,15 +1,22 @@
 import { medusaIntegrationTestRunner } from "@medusajs/test-utils"
+import { shouldRunPgIntegration } from "./_helpers"
 jest.setTimeout(60 * 1000)
 
-medusaIntegrationTestRunner({
-  inApp: true,
-  env: {},
-  testSuite: ({ api }) => {
-    describe("Ping", () => {
-      it("ping the server health endpoint", async () => {
-        const response = await api.get('/health')
-        expect(response.status).toEqual(200)
+if (shouldRunPgIntegration()) {
+  medusaIntegrationTestRunner({
+    inApp: true,
+    env: {},
+    testSuite: ({ api }) => {
+      describe("Ping", () => {
+        it("ping the server health endpoint", async () => {
+          const response = await api.get('/health')
+          expect(response.status).toEqual(200)
+        })
       })
-    })
-  },
-})
+    },
+  })
+} else {
+  describe("Ping (skipped)", () => {
+    it.skip("requires Postgres; set RUN_PG_TESTS=1", () => {})
+  })
+}
