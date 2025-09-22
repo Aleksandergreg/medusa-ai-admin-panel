8d031cafb72442cfd9c26fe1ced0d264daf8cf20
AndersBondeLudvigsen <anlu001@stud.kea.dk>
2025-09-22 13:27:26 +0200

    removed abandoned cart feature

--- my-medusa-store/src/modules/abandoned-carts/index.ts
+++
@@ -1,8 +0,0 @@
-import AbandonedCartsService from "./service"
-import { Module } from "@medusajs/framework/utils"
-
-export const ABANDONED_CARTS_MODULE = "abandoned_carts"
-
-export default Module(ABANDONED_CARTS_MODULE, {
-  service: AbandonedCartsService,
-})
